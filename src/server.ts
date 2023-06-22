0739152ea76d1d6cbf746d98d3dc5f6122c5217b
kagemth73 <srjkk8234@yahoo.co.jp>
2023-06-22 21:44:08 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import http from "node:http";
 import * as fs from "node:fs";
+import { log } from "node:console";
 const server = http.createServer();
 const port = process.env.PORT ?? 12345;
 const path = require("path");
@@ -9,35 +10,35 @@ server.on("request", async (req, res) => {
   // Content-Type is important for browsers.
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
 
-  let filePath = './public' + req.url;
-  // if(filePath === './public/'){
-  //   filePath = ''
-  // }
+  let filePath = "./public" + req.url;
+  if (filePath === "./public/") {
+    filePath = "./public/index.html";
+  }
 
   let extname = String(path.extname(filePath)).toLocaleLowerCase();
-  let mimeTypes: Record<string, string> = {
-    '.html': 'text/html',
-    '.jpg': 'image/jpeg',
-    '.json': 'text/json',
-    '.ico': 'image/x-ico'
+  let mimeTypes: Record<string, string | undefined> = {
+    ".html": "text/html",
+    ".jpg": "image/jpeg",
+    ".json": "text/json",
+    ".ico": "image/x-ico",
   };
-  let contentType = mimeTypes[extname] || 'application/octet-stream';
+  let contentType = mimeTypes[extname] || "application/octet-stream";
 
   fs.readFile(filePath, (error, content) => {
-    if(error){
-      if(error.code == 'ENOENT'){
+    if (error) {
+      if (error.code == "ENOENT") {
         res.writeHead(404);
+        console.log("404 NOT FOUND");
         res.end();
       } else {
         res.writeHead(500);
         res.end();
       }
     } else {
-      res.writeHead(200, {"Content-Type": contentType});
-      res.end();
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
     }
   });
-
 });
 
 server.on("listening", () => {
